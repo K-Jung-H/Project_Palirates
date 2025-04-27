3fdba58aee7e8f804d1e7d15a7c56d6417c4c9cc
dlarlwns135 <dlarlwns135@naver.com>
2025-04-27 12:55:33 +0900

    test

--- Palirates/Palirates/GameFramework.cpp
+++ Palirates/Palirates/GameFramework.cpp
@@ -618,7 +618,7 @@ void CGameFramework::Build_Scenes()
 	scene_manager->Set_Scene_Player("Character_Select", select_scene_observer);
 
 
-	scene_manager->Set_Active_Scene("Character_Select");
+	scene_manager->Set_Active_Scene("In_Stage");
 	m_pPlayer = scene_manager->Get_Active_Scene_Player();
 	m_pPlayer->SetPosition(XMFLOAT3{ 50.0f, 0.0f, 50.0f });
 
