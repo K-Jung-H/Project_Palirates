b153519d05ba461a637a2935923974c4480bb08f
KJH <jay5136@naver.com>
2025-07-27 23:25:43 +0900

    work

--- Palirates/Palirates/GameFramework.cpp
+++ Palirates/Palirates/GameFramework.cpp
@@ -660,6 +660,9 @@ bool CGameFramework::Change_Scene()
 	if (!c_signal.change)
 		return false;
 
+	if (c_signal.type == Scene_Type::Lobby)
+		Character_Select_Scene::Reset_Select();
+
 	if (c_signal.type == Scene_Type::Board)
 		Board_Scene::Reset_Sail_Status();
 	if (scene_manager->Find_Scene(c_signal.scene_name))
--- Palirates/Palirates/Scene.h
+++ Palirates/Palirates/Scene.h
@@ -310,7 +310,7 @@ public:
 class Character_Select_Scene : public CScene
 {
 private:
-	int is_Ready = 0; 
+	static int is_Ready; 
 
 
 	std::array<int, MaxPlayer> readyClientIds;
@@ -345,6 +345,8 @@ public:
 
 	void SetReadyClientIds(const std::array<int, MaxPlayer>& readyIds) { readyClientIds = readyIds; }
 	void SetCharacterSelections(const std::array<std::bitset<MaxPlayer>, MaxPlayer>& selections) { characterSelections = selections; }
+
+	static void Reset_Select() { select_index = 0; is_Ready = false; }
 };
 
 class Board_Scene : public CScene
