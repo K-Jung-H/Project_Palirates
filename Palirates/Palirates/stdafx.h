8fe27584cd684622cff35e779648814f18df4d1d
zhaominn <jay5136@naver.com>
2025-07-28 09:53:40 +0900

    Update

--- Palirates/Palirates/stdafx.h
+++ Palirates/Palirates/stdafx.h
@@ -99,6 +99,7 @@ extern bool isRunning;
 #define FRAME_BUFFER_WIDTH				1280
 #define FRAME_BUFFER_HEIGHT				720
 
+
 #define ANIMATION_TYPE_ONCE				0
 #define ANIMATION_TYPE_LOOP				1
 #define ANIMATION_TYPE_PINGPONG			2
@@ -594,8 +595,8 @@ enum class Monster_Type : int
 //#define SERVER_IP "1.242.69.251"
 //#define SERVER_IP "192.168.69.73"
 //#define SERVER_IP "172.30.1.13"
-#define SERVER_IP "127.0.0.1"
-//#define SERVER_IP "10.30.2.21"
+//#define SERVER_IP "127.0.0.1"
+#define SERVER_IP "10.30.2.21"
 //#define SERVER_IP "192.168.159.1"
 
 #define SERVER_PORT 9000
